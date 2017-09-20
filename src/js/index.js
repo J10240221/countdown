57e35399d78b517d997d67ac2d41fab563b4a7d2
huazhilin <zhilingege@gmail.com>
2017-09-20 11:31:18 +0800

    换图片

--- src/js/index.js
+++ src/js/index.js
@@ -14,7 +14,8 @@ const Container = () => {
             该组件传递给子组件的属性有：<br />
             <strong> endTime="2017/09/21 00:00:00" </strong><br />
             <strong> myCorrNowTime="2017/09/20 00:00:00" </strong><br />
-            所以每次刷新页面都是24小时开始倒计时，其实这个<strong>myCorrNowTime</strong>属性的使用场景，就是在需要使用的服务器时间的场景，避免用户客户端的时间不对，而造成同一个时刻，不同用户显示的倒计时不一致的问题；
+            所以每次刷新页面都是24小时开始倒计时，其实这个<strong>myCorrNowTime</strong>属性的使用场景，就是在需要使用的<br/>
+            服务器时间的场景，避免用户客户端的时间不对，而造成同一个时刻，不同用户显示的倒计时不一致的问题；
             <hr />
             这两个属性的用法如下：<br />
             <strong> endTime="倒计时的最后时刻" </strong><br />
