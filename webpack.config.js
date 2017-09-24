12013de767ea01b3655dae7a2b50b288a83bae40
华志林 <zhilingege@gmail.com>
2017-09-24 15:33:25 +0800

    更改整体项目包

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,37 @@
-var debug = process.env.NODE_ENV !== "production";
-var webpack = require('webpack');
-var path = require('path');
-
 module.exports = {
-  context: path.join(__dirname),
-  devtool: debug ? "inline-sourcemap" : null,
-  entry: "./src/js/index.js",
-  module: {
-    loaders: [
-      {
-        test: /\.js?$/,
-        exclude: /(node_modules)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015']
-        }
-      }
-    ]
-  },
-  output: {
-    path: __dirname,
-    filename: "./src/bundle.js"
-  },
-  plugins: debug ? [] : [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
-  ],
-};
+   entry: __dirname + '/app/main.js',
+   output: {
+      path: __dirname + '/public',
+      filename: 'bundle.js'
+   },
+
+   devtool: 'cheap-module-source-map',
+
+   devServer: {
+      contentBase: './public',//本地服务器所加载的页面所在的目录
+      historyApiFallback: true,//不跳转
+      inline: true,//实时刷新
+   },
+
+   module: {
+      rules: [
+         {
+            test: /(.js[x]?)$/,
+            use: {
+               loader: "babel-loader",
+            },
+            exclude: /node_modules/
+         },
+         {
+            test: /.css$/,
+            use: [
+               {
+                  loader: "style-loader"
+               },{
+                  loader: "css-loader"
+               }
+            ]
+         }
+      ]
+   }
+}
